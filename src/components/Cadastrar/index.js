c7a05547c1cff77f345c4f9890762f9c7c2c4e1f
Eduardo <edr.ccol@gmail.com>
2019-11-05 11:24:10 -0300

    Add files via upload

---
+++ src/components/Cadastrar/index.js
@@ -0,0 +1,72 @@
+import React, { useState, Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+  Animated,
+  Image,
+} from 'react-native';
+
+export default class Cadastrar extends Component{
+  render() {
+    return (
+        <SafeAreaView style={styles.container}>
+
+          <StatusBar backgroundColor="#DBA901" barStyle="light-content"></StatusBar>
+        
+          <View>
+              <Button 
+              title="Back"
+              onPress={_ => this.props.navigation.goBack()}
+              />
+          </View>
+        </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FACC2E"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+    color: "#FFF",
+    fontWeight: "bold"
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  button: {
+    width: 300,
+    justifyContent: "center"
+  },
+  cards: {
+    color: '#FBE37B',
+    fontSize: 30,
+    marginTop: 12,
+    fontFamily: 'Montserrat-Regular',
+
+  },
+  nameCards: {
+    backgroundColor: '#282928',
+    borderRadius: 20,
+    width: 200,
+    height: 70,
+    marginTop: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
+});
